89d04751d35ed1430ba4c34b981a2f45a0c1e185
codewithash <codewithash.dev@gmail.com>
2025-12-16 10:55:36 +0200

    Django project created, Apps structured properly, MySQL connected

--- digital_school/settings.py
+++ digital_school/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'digital_school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'digital_school_db',
+        'USER': 'school_user',
+        'PASSWORD': 'hymfih-3pYzze-vatpoh',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
